919968faaa4290c8f28953ffb9e8d76d58b333db
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2023-10-29 15:13:24 +0100

    Ajout logo,copyright. Refactor accueil

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,30 @@
 document.getElementById("flag").play();
 const menuList = document.getElementById("menuList");
+const content = document.getElementById("content");
+for (const child of content.children)
+{
+    console.log(child);
+    child.addEventListener("click", ()=>{
+        let isShow = true;
+        for(const nameClass of child.classList)
+        {
+            if(nameClass !== "show") continue;
+            isShow = false;
+            break;
+        }
+        if(isShow)
+            child.classList.add("show");
+        else
+            child.classList.remove("show");
+    });
+}
+
 function show() {
     const isVisible = menuList.style.display === "block";
     const nav = menuList.children[0];
     menuList.style.display = isVisible ? "none" : "block";
-    
-    if(isVisible){
+
+    if (isVisible) {
         nav.style.display = "none";
         return;
     }
