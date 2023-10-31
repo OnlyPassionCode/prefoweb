9047e33cf6d037c71419589e0442039fd27b1f01
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2023-10-31 15:42:29 +0100

    Image index, refactor js, showingdiv

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,35 @@
 document.getElementById("flag").play();
 const menuList = document.getElementById("menuList");
-const content = document.getElementById("content");
-for (const child of content.children)
-{
-    const sizeBorderBottom = 1;
-    const sizeClosed = (child.children[0].scrollHeight + sizeBorderBottom) + "px";
-    child.style.maxHeight = sizeClosed;
-    child.children[0].addEventListener("click", ()=>{
-        let isClosed = child.style.maxHeight === sizeClosed;
-        if(isClosed){
-            child.style.maxHeight = child.scrollHeight + "px";
-        }
-        else{
-            child.style.maxHeight = (child.children[0].scrollHeight + sizeBorderBottom) + "px";
-        }
-    });
+const showingDiv = document.getElementsByClassName("showingDiv");
+const sizeBorderBottom = 1;
+const sizeClosed =
+  showingDiv[0].children[0].scrollHeight + sizeBorderBottom + "px";
+let lastShow;
+for (const section of showingDiv) {
+  section.style.maxHeight = sizeClosed;
+  section.children[0].addEventListener("click", () => {
+    if (lastShow !== undefined) {
+      lastShow.style.maxHeight = sizeClosed;
+      const needToReturn = lastShow === section;
+      lastShow = undefined;
+      if (needToReturn) return;
+    }
+    lastShow = section;
+    section.style.maxHeight = section.scrollHeight + "px";
+  });
 }
 
 function show() {
-    const isVisible = menuList.style.display === "block";
-    const nav = menuList.children[0];
-    menuList.style.display = isVisible ? "none" : "block";
+  const isVisible = menuList.style.display === "block";
+  const nav = menuList.children[0];
+  menuList.style.display = isVisible ? "none" : "block";
 
-    if (isVisible) {
-        nav.style.display = "none";
-        return;
-    }
+  if (isVisible) {
+    nav.style.display = "none";
+    return;
+  }
 
-    setTimeout(() => {
-        nav.style.display = "block";
-    }, 450);
+  setTimeout(() => {
+    nav.style.display = "block";
+  }, 450);
 }
