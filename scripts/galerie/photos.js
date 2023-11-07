e043a9cb5ad33e4f085b07da3b70b4ac3c0a8b77
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2023-11-07 15:11:51 +0100

    Animation image timeline galerie

--- scripts/galerie/photos.js
+++ scripts/galerie/photos.js
@@ -10,24 +10,91 @@ fileName.sort(function (a, b) {
   return a - b;
 });
 
-const folder = "images/timelineResized/";
-const timeline = document.getElementById("timeline").children[1];
-const showImg = document.getElementById("viewImage");
-const imgShowImg = document.getElementById("imgViewImage");
-const backgroundViewImage = document.getElementById("backgroundViewImage");
+function getCurrentTranslateXFromElement(element) {
+  return window
+    .getComputedStyle(element, null)
+    .getPropertyValue("transform")
+    .split(",")[4]
+    .trim();
+}
 
+function stopTranslateAndKeepPosition(currentTranslateX) {
+  for (let y = 0; y < timeline.children.length; ++y) {
+    const image = timeline.children[y];
+    image.style.transform = "translate(" + currentTranslateX + "px, 0)";
+    image.classList.add("notransition");
+  }
+}
+
+function restartTranslateWithCurrentPosition(currentTranslateX) {
+  const widthScroll = timeline.children[0].scrollWidth;
+  for (let y = 0; y < timeline.children.length; ++y) {
+    const image = timeline.children[y];
+    image.classList.remove("notransition");
+    image.style.transform = "translate(-" + totalScrollWidth + "px, 0)";
+    const duration =
+      120 - (Math.floor(Math.abs(+currentTranslateX)) / widthScroll) * 3;
+    image.style.transitionDuration = duration + "s";
+  }
+}
+
+const showImg = document.getElementById("viewImage");
+const backgroundViewImage = document.getElementById("backgroundViewImage");
+const timeline = document.getElementById("timeline").children[1];
+let isClickedImage = false;
 backgroundViewImage.addEventListener("click", () => {
   showImg.style.display = "none";
   backgroundViewImage.style.display = "none";
+  restartTranslateWithCurrentPosition(
+    getCurrentTranslateXFromElement(timeline.children[0])
+  );
+  isClickedImage = false;
 });
 
-for (let i = 0; i < 6; ++i) {
-  const img = document.createElement("img");
-  img.src = folder + fileName[i] + ".jpg";
-  img.addEventListener("click", () => {
-    imgShowImg.src = img.src.replace("timelineResized", "timeline");
-    showImg.style.display = "block";
-    backgroundViewImage.style.display = "block";
-  });
-  timeline.appendChild(img);
+function loadBaseImage() {
+  const folder = "images/timelineResized/";
+  const imgShowImg = document.getElementById("imgViewImage");
+  for (let i = 0; i < names.length; ++i) {
+    const img = document.createElement("img");
+    img.src = folder + fileName[i] + ".jpg";
+    img.addEventListener("click", () => {
+      isClickedImage = true;
+      imgShowImg.src = img.src.replace("timelineResized", "timeline");
+      showImg.style.display = "block";
+      backgroundViewImage.style.display = "block";
+      stopTranslateAndKeepPosition(getCurrentTranslateXFromElement(img));
+    });
+    timeline.appendChild(img);
+  }
 }
+loadBaseImage();
+
+let totalScrollWidth = 0;
+function animation() {
+  for (let i = 0; i < timeline.children.length; ++i) {
+    const scrollWidth = timeline.children[i].scrollWidth;
+    totalScrollWidth += scrollWidth;
+  }
+  for (let i = 0; i < timeline.children.length; ++i) {
+    timeline.children[i].style.transform =
+      "translate(-" + totalScrollWidth + "px, 0)";
+  }
+}
+animation();
+
+function addEventListenerToImage() {
+  for (let i = 0; i < timeline.children.length; ++i) {
+    const overImage = timeline.children[i];
+    overImage.addEventListener("mouseover", () => {
+      const currentTranslateX = getCurrentTranslateXFromElement(overImage);
+      stopTranslateAndKeepPosition(currentTranslateX);
+    });
+    overImage.addEventListener("mouseout", () => {
+      if (isClickedImage) return;
+      restartTranslateWithCurrentPosition(
+        getCurrentTranslateXFromElement(overImage)
+      );
+    });
+  }
+}
+addEventListenerToImage();
