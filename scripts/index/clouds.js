75ddab24b85b53a0f33470aeb3981c7775e27825
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2023-11-10 13:36:34 +0100

    Refactoring + card profil

--- scripts/index/clouds.js
+++ scripts/index/clouds.js
@@ -1,10 +1,12 @@
-let idCloud = 1;
-const pathCloud = "images/clouds/cloud";
+function activateClouds(seconds) {
+  let idCloud = 1;
+  const pathCloud = "images/clouds/cloud";
 
-const imgCloud = document.getElementById("cloud").children[0];
-imgCloud.src = pathCloud + idCloud + ".png";
-setInterval(() => {
-  ++idCloud;
-  if (idCloud > 5) idCloud = 1;
+  const imgCloud = document.getElementById("cloud").children[0];
   imgCloud.src = pathCloud + idCloud + ".png";
-}, 22 * 1000);
+  setInterval(() => {
+    ++idCloud;
+    if (idCloud > 5) idCloud = 1;
+    imgCloud.src = pathCloud + idCloud + ".png";
+  }, seconds * 1000);
+}
