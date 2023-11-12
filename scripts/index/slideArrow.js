567674859b6aff391f1990870df5d721b985d722
OnlyPassionCode <van.wassenhove.kevin.pro@hotmail.com>
2023-11-12 16:17:22 +0100

    Rectification position scroll slideArrow inddex

--- scripts/index/slideArrow.js
+++ scripts/index/slideArrow.js
@@ -2,7 +2,7 @@ const arrowIndiquation = document.getElementById("arrowIndiquation");
 arrowIndiquation.addEventListener("click", () => {
   const index = document.getElementById("index");
   window.scroll({
-    top: index.scrollHeight - 120,
+    top: index.scrollHeight - 160,
     left: 0,
     behavior: "smooth",
   });
